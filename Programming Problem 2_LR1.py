3ede93d33be7605817d672e2c4b412916a5f12ff
Umadac, Christian Luis <156592818+chrimiclu782@users.noreply.github.com>
2024-12-15 00:19:44 +0800

    Update Programming Problem 2_LR1.py

--- Programming Problem 2_LR1.py
+++ Programming Problem 2_LR1.py
@@ -11,3 +11,13 @@ for line in fin:
 
 	fileList.append(line)
 
+cont = True
+while cont:
+	print('The file has',len(fileList),'lines.')
+	lineNo = int(input('Enter a line number [0 to quit]: '))
+	if lineNo == 0:
+		cont = False
+	else:
+		print(str(lineNo)+': '+fileList[lineNo-1]+'\n')
+
+fin.close()
