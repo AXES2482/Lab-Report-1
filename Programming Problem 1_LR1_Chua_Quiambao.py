6dd905ac319ba4f5c9ab4afd755fa6fe6ab0cc22
AndreaQuiambao <156178864+AndreaQuiambao@users.noreply.github.com>
2024-12-15 00:26:43 +0800

    Update and rename Problem1_Chua to Programming Problem 1_LR1_Chua_Quiambao.py

--- Programming Problem 1_LR1_Chua_Quiambao.py
+++ Programming Problem 1_LR1_Chua_Quiambao.py
@@ -16,3 +16,23 @@ def mean(lyst):
 def mode(lyst):
     mode1=max(lyst,key=lyst.count)
     return mode1
+
+def main():
+
+    lyst = []
+
+    for i in range(6):
+
+      lyst.append(int(input('Enter a Value for the list: ')))
+
+    print("List:", lyst)
+
+    print("Mode:", mode(lyst))
+
+    print("Median:", median(lyst))
+
+    print("Mean:", mean(lyst))
+
+if __name__ == "__main__":
+
+    main()
